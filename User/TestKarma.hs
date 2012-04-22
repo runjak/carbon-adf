124859ed33634fa9e08de72ead880e777403ccce
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-04-22 18:13:03 +0200

    Added module User.Karma for a Karma newtype.
    Ignore-this: 6dda3c00d66e5a25fe5cc82198928931
    
    - This comes with User.TestKarma.
    - Karma cannot be <0.
    
    darcs-hash:20120422161303-d180a-e1183798fa8fccc1096cff396b422ac33ff79247

---
+++ User/TestKarma.hs
@@ -0,0 +1,25 @@
+module User.TestKarma (test) where
+
+import Test.QuickCheck as T
+import qualified User.Karma as K
+
+test = do
+  putStrLn "Testing User.Karma"
+  quickCheck subtraction
+  quickCheck fromto
+  quickCheck hasK
+  where
+    subtraction :: (Int, Int) -> Bool
+    subtraction (a, b) = do
+      let x = K.toKarma a
+      let y = K.toKarma b
+      x - y >= 0
+    fromto :: Int -> Bool
+    fromto x = (max 0 x) == K.fromKarma (K.toKarma x)
+    hasK :: (Int, Int) -> Bool
+    hasK (x, y) = do
+      let a = max 0 x
+      let b = max 0 y
+      (a <= b) == (K.hasKarma a $ K.toKarma b)
+
+main = test
