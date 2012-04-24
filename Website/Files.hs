b4b143638ee677e54080b59dac066b9d7948242d
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-04-24 16:33:00 +0200

    Created module Website.Files
    Ignore-this: 6ee1f3d3b1f95e5a20a8169bb41be888
    
    - It uses the config to serve static files under the path <website>/files/
    
    darcs-hash:20120424143300-d180a-6c96bc4c363034d549a6434a66e3ea493cbf3d13

---
+++ Website/Files.hs
@@ -0,0 +1,23 @@
+module Website.Files (serve) where
+{-
+  This module serves the static files on the website
+  which are located in /files/.
+  It uses the Config.File module for configuration.
+-}
+import Config hiding (nullConfig)
+import Happstack.Server (Browsing(..), serveDirectory, dir)
+import qualified Happstack.Server as H
+
+serve conf = do
+  let b = browsing $ allowBrowsing conf
+  dir "files" . serveDirectory b fallbackFiles $ fileStorage conf
+
+browsing :: Bool -> Browsing
+browsing True   = EnableBrowsing
+browsing False  = DisableBrowsing
+
+{-
+  serve will try to serve these if the requested file is not found.
+-}
+fallbackFiles :: [String]
+fallbackFiles = ["index.html"]
