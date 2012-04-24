5843fd86aa6d9f67409a2614b3e1310de0308e85
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-04-24 22:20:21 +0200

    Created module Website.Basic for things shared between the Website modules.
    Ignore-this: 19c6ddd51089e51c8c6d82c9c809c2e
    
    darcs-hash:20120424202021-d180a-84995be829e1114a582e919132502b8850eb2b27

---
+++ Website/Basic.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Website.Basic (
+    template, template'
+  , keywords, keywords'
+) where
+{-
+  Basic functions that will help in different parts of the Website.
+-}
+import Text.Blaze ((!))
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
+type Title = String
+type Meta = [H.Html]
+type Body = H.Html
+template :: Title -> Meta -> Body -> H.Html
+template t m b = do
+  H.docType
+  H.html $ do
+    H.head $ do
+      H.title $ H.toHtml t
+      H.meta ! A.httpEquiv "Content-Type" ! A.content "text/html;charset=utf-8"
+      sequence_ m
+    H.body b
+
+template' :: Title -> Body -> H.Html
+template' t b = template t [] b
+
+-- | Keywords for meta tags should be comma seperated
+type Keywords = String
+keywords :: Keywords -> Meta
+keywords k = [H.meta ! A.name (H.toValue ("keywords" :: String)) ! A.content (H.toValue k)]
+
+keywords' :: Meta -> Keywords -> Meta
+keywords' ms k = keywords k ++ ms
