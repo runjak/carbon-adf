56a17728b9416b84aa2565e99da9f6d7ddb7a378
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-06-26 17:52:54 +0200

    Wip for class based Backend
    Ignore-this: 9fad9ef4f0e0b558b7009084730f91d9
    
    darcs-hash:20120626155254-d180a-7b1e7c595b8e1e05f213370a7e3d3de8ab8b101f

--- OpenBrain/Backend.hs
+++ OpenBrain/Backend.hs
@@ -1,15 +1,11 @@
-module OpenBrain.Backend (
-    Backend(..)
-  , UserBackend(..)
-  , ProfileBackend(..)
-  , KarmaBackend(..)
-  , SaltShaker(..)
-  , SessionManagement(..), ActionKey
-) where
+{-# LANGUAGE ExistentialQuantification #-}
+module OpenBrain.Backend where
 {-
   This module provides the Backend class that will be used to generate the website.
   The Backend will provide things like Userdata :P
 -}
+import Control.Monad.Trans.Maybe
+
 import OpenBrain.Config
 import OpenBrain.Data.User
 import OpenBrain.Data.Hash (Hash)
@@ -19,41 +15,73 @@ import OpenBrain.Data.Profile hiding (emptyProfile)
 import OpenBrain.Data.Salt (Salt)
 
 {- The highest abstraction of the backend-tree. -}
-data Backend = Backend {
-    shutdown          :: IO ()
-  , userBackend       :: UserBackend
-  , karmaBackend      :: KarmaBackend
-  , saltShaker        :: SaltShaker
-  , sessionManagement :: SessionManagement
-}
+class Backend b where
+  shutdown          :: b -> IO ()
+  userBackend       :: b -> CUserBackend
+  karmaBackend      :: b -> CKarmaBackend
+  saltShaker        :: b -> CSaltShaker
+  sessionManagement :: b -> CSessionManagement
+data CBackend = forall b . Backend b => CBackend b
+instance Backend CBackend where
+  shutdown (CBackend b)           = shutdown b
+  userBackend (CBackend b)        = userBackend b
+  karmaBackend (CBackend b)       = karmaBackend b
+  saltShaker (CBackend b)         = saltShaker b
+  sessionManagement (CBackend b)  = sessionManagement b
 
 {- Controls for everything userrelated. -}
-data UserBackend = UserBackend {
-    login           :: UserName -> Hash -> IO (Maybe UserData) -- The Backend will update the lastLogin in UserData.
-  , getUser         :: UserId -> IO (Maybe UserData)
-  , hasUserWithId   :: UserId -> IO Bool
-  , hasUserWithName :: UserName -> IO (Maybe UserId)
-  , register        :: UserName -> Hash -> IO (Maybe UserData) -- The Backend will check for duplicate UserNames.
-  , delete          :: UserId -> IO Bool
-  , profileBackend  :: ProfileBackend
-  , getUserList     :: IO [UserId] -- FIXME Offset, Limit, Count
-  , updateKarma     :: UserId -> (Karma -> Karma) -> IO ()
-  , updatePasswd    :: UserId -> Hash -> IO ()
-  , setAdmin        :: UserId -> Bool -> IO ()
-}
+type Limit  = Int
+type Offset = Int
+class UserBackend u where
+  login           :: u -> UserName -> Hash -> MaybeT IO UserData -- The Backend will update the lastLogin in UserData.
+  getUser         :: u -> UserId -> MaybeT IO UserData
+  hasUserWithId   :: u -> UserId -> IO Bool
+  hasUserWithName :: u -> UserName -> MaybeT IO UserId
+  register        :: u -> UserName -> Hash -> MaybeT IO UserData -- The Backend will check for duplicate UserNames.
+  delete          :: u -> UserId -> IO Bool
+  profileBackend  :: u -> CProfileBackend
+  getUserCount    :: u -> IO Int
+  getUserList     :: u -> Limit -> Offset -> IO [UserId]
+  updateKarma     :: u -> UserId -> (Karma -> Karma) -> IO ()
+  updatePasswd    :: u -> UserId -> Hash -> IO ()
+  setAdmin        :: u -> UserId -> Bool -> IO ()
+data CUserBackend = forall u . UserBackend u => CUserBackend u
+instance UserBackend CUserBackend where
+  login (CUserBackend u)            = login u
+  getUser (CUserBackend u)          = getUser u
+  hasUserWithId (CUserBackend u)    = hasUserWithId u
+  hasUserWithName (CUserBackend u)  = hasUserWithName u
+  register (CUserBackend u)         = register u
+  delete (CUserBackend u)           = delete u
+  profileBackend (CUserBackend u)   = profileBackend u
+  getUserCount (CUserBackend u)     = getUserCount u
+  getUserList (CUserBackend u)      = getUserList u
+  updateKarma (CUserBackend u)      = updateKarma u
+  updatePasswd (CUserBackend u)     = updatePasswd u
+  setAdmin (CUserBackend u)         = setAdmin u
 
 {- Controls for Userprofiles. -}
-data ProfileBackend = ProfileBackend {
-    getProfileId        :: UserId -> IO ProfileId
-  , getProfile          :: ProfileId -> IO (Maybe Profile)
-  , setAccessRule       :: ProfileId -> AccessRule -> IO ()
-  , setName             :: ProfileId -> Maybe Name -> IO ()
-  , setAvatar           :: ProfileId -> Maybe String -> IO ()
-  , setLocations        :: ProfileId -> [Location] -> IO ()
-  , setWebsites         :: ProfileId -> [ProfileSnippet] -> IO ()
-  , setEmails           :: ProfileId -> [ProfileSnippet] -> IO ()
-  , setInstantMessagers :: ProfileId -> [ProfileSnippet] -> IO ()
-}
+class ProfileBackend p where
+  getProfileId        :: p -> UserId -> IO ProfileId
+  getProfile          :: p -> ProfileId -> MaybeT IO Profile
+  setAccessRule       :: p -> ProfileId -> AccessRule -> IO ()
+  setName             :: p -> ProfileId -> Maybe Name -> IO ()
+  setAvatar           :: p -> ProfileId -> Maybe String -> IO ()
+  setLocations        :: p -> ProfileId -> [Location] -> IO ()
+  setWebsites         :: p -> ProfileId -> [ProfileSnippet] -> IO ()
+  setEmails           :: p -> ProfileId -> [ProfileSnippet] -> IO ()
+  setInstantMessagers :: p -> ProfileId -> [ProfileSnippet] -> IO ()
+data CProfileBackend = forall p . ProfileBackend p => CProfileBackend p
+instance ProfileBackend CProfileBackend where
+  getProfileId (CProfileBackend p)        = getProfileId p
+  getProfile (CProfileBackend p)          = getProfile p
+  setAccessRule (CProfileBackend p)       = setAccessRule p
+  setName (CProfileBackend p)             = setName p
+  setAvatar (CProfileBackend p)           = setAvatar p
+  setLocations (CProfileBackend p)        = setLocations p
+  setWebsites (CProfileBackend p)         = setWebsites p
+  setEmails (CProfileBackend p)           = setEmails p
+  setInstantMessagers (CProfileBackend p) = setInstantMessagers p
 
 {-
   Controls for everything karma related.
@@ -61,21 +89,28 @@ data ProfileBackend = ProfileBackend {
   for any amount of karma.
   These are IO so that necessary karma can be computed.
 -}
-data KarmaBackend = KarmaBackend {
-    karmaDeleteUser :: IO Karma
-  , karmaEditUser   :: IO Karma
-}
+class KarmaBackend k where
+  karmaDeleteUser :: k -> IO Karma
+  karmaEditUser   :: k -> IO Karma
+data CKarmaBackend = forall k . KarmaBackend k => CKarmaBackend k
+instance KarmaBackend CKarmaBackend where
+  karmaDeleteUser (CKarmaBackend k) = karmaDeleteUser k
+  karmaEditUser (CKarmaBackend k)   = karmaEditUser k
 
 {-
   The SaltShaker produces Salts
   that can be bound to a UserId
   by using setId.
 -}
-data SaltShaker = SaltShaker {
-    setId :: Salt -> UserId -> IO () -- Stores a tuple of Salt and Id in the Backend.
-  , getSalt :: UserId -> IO Salt -- May use shake and setId of no Salt exists.
-  , removeSalt :: UserId -> IO () -- Deleting the Salt when it's not necessary anymore.
-}
+class SaltShaker s where
+  setId       :: s -> Salt -> UserId -> IO ()
+  getSalt     :: s -> UserId -> IO Salt
+  removeSalt  :: s -> UserId -> IO ()
+data CSaltShaker = forall s . SaltShaker s => CSaltShaker s
+instance SaltShaker CSaltShaker where
+  setId (CSaltShaker s)       = setId s
+  getSalt (CSaltShaker s)     = getSalt s
+  removeSalt (CSaltShaker s)  = removeSalt s
 
 {-
   SessionManagement helps managing logged in clients.
@@ -85,9 +120,15 @@ data SaltShaker = SaltShaker {
   If the action is valid a new ActionKey will be produced to be stored instead of the old one.
 -}
 type ActionKey = String
-data SessionManagement = SessionManagement {
-    startSession  :: UserId -> IO ActionKey
-  , validate      :: UserId -> ActionKey -> IO Bool
-  , perform       :: UserId -> ActionKey -> IO (Maybe ActionKey)
-  , stopSession   :: UserId -> ActionKey -> IO ()
-}
+class SessionManagement s where
+  startSession  :: s -> UserId -> IO ActionKey
+  validate      :: s -> UserId -> ActionKey -> IO Bool
+  perform       :: s -> UserId -> ActionKey -> MaybeT IO ActionKey
+  stopSession   :: s -> UserId -> ActionKey -> IO ()
+data CSessionManagement = forall s . SessionManagement s => CSessionManagement s
+instance SessionManagement CSessionManagement where
+  startSession (CSessionManagement s) = startSession s
+  validate (CSessionManagement s)     = validate s
+  perform (CSessionManagement s)      = perform s
+  stopSession (CSessionManagement s)  = stopSession s
+
