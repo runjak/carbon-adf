9d22cc54035065c26486a1f4c094a814b65f6213
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-06-26 15:38:01 +0200

    Added first .svg files in files/img
    Ignore-this: 455aede47d4fff839125292832c094e9
    
    darcs-hash:20120626133801-d180a-4f834f2304a2eec7a53171c1da545198c2a7807d

---
+++ OpenBrain/Website/Images.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+module OpenBrain.Website.Images (
+    add, clear, edit
+  , logout, remove, save
+  , users
+) where
+{-
+  Defines easy to replace image locations and syntax to embed them.
+-}
+import Text.Blaze ((!))
+import Text.Blaze.Html (ToMarkup(..))
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
+path :: String -> H.AttributeValue
+path = H.toValue . ("files/img/" ++)
+
+image :: String -> H.Html
+image i = H.embed ! A.type_ "image/svg+xml" ! A.src (path i)
+
+add     = image "add.svg"
+clear   = image "clear.svg"
+edit    = image "edit.svg"
+logout  = image "logout.svg"
+remove  = image "remove.svg"
+save    = image "save.svg"
+users   = image "users.svg"
