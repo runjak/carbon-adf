46e225699d8585606eb4a5eaf74b3a160729ffa0
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-08-06 19:39:27 +0200

    Refined OpenBrain.Data.Information
    Ignore-this: 2e8ec03e91a4b38c066a7fc403135455
    
    - Simplified Decision, Discussion and Collection into Collection carrying a type.
      This leads to the next step of building a new Schema for it.
    
    darcs-hash:20120806173927-d180a-368fd7491b84e9b6dca2bf046d4b8b26e8b3d009

--- OpenBrain/Data/Information.hs
+++ OpenBrain/Data/Information.hs
@@ -8,30 +8,39 @@ import qualified OpenBrain.Data.User as User
 
 type InformationId = Id
 data Information = Information {
-    author        :: User.UserData
-  , creation      :: CalendarTime
-  , description   :: String
+    author        :: User.UserData  -- | Every information has an author.
+  , creation      :: CalendarTime   -- | The time of creation for the information.
+  , description   :: String         -- | A short description of what the information is about.
   , informationId :: InformationId
-  , media         :: Media
-  , title         :: String
+  , media         :: Media          -- | The real content
+  , title         :: String         -- | A title for the information
   } deriving (Eq, Show)
 
-type Voted = Bool
 data Media =
-    Media String -- | HTML rich content
-  | Collection [Information]
-  | Discussion {
-    arguments     :: [Information]            -- | Arguments that make up the Discussion
-  , afType        :: ArgumentationFramework   -- | Choosen on creation
-  , choices       :: [([Information], Int)]   -- | Groups of Informations with their vote count
-  , complete      :: Maybe Information        -- | Media of that Information should always be a Decision
-  , deadline      :: CalendarTime             -- | After this no changes to arguments are allowed any longer
-  , participants  :: [(User.UserData, Voted)] -- | Anyone that decided to be a participant before the deadline
+    Content String  -- | HTML rich content
+  | Collection {    -- | Everything that is not a single information
+    arguments       :: [Information]        -- | Informations that are grouped by this Collection.
+  , collectionType  :: CollectionType       -- | The nature of this Collection.
+  , discussion      :: Maybe DiscussionInfo -- | Additional Information if the Collection is a Discussiontype.
   }
-  | Decision [Information]
   deriving (Eq, Show)
 
-data ArgumentationFramework = AttackOnly | AttackDefense deriving (Show, Read, Eq, Enum)
+data CollectionType =       -- | The different kinds of collections of informations in OpenBrain
+    SimpleCollection        -- | Information that is grouped by a user
+  | Choice                  -- | A possible result of a discussion
+  | Decision                -- | The winning choice of a discussion
+  | DiscussionAttackOnly    -- | A discussion in which only attacks count.
+  | DiscussionAttackDefense -- | A discussion that considers attacks and defenses.
+  deriving (Show, Read, Eq, Enum)
+
+type Votes = Int
+type Voted = Bool
+data DiscussionInfo = DiscussionInfo {        -- | If a collection is a discussion it should have discussioninfo with it.
+    choices       :: [(Information, Votes)]   -- | A list of possible choices along with the votes they get.
+  , complete      :: Maybe Information        -- | Target information should be a Decision.
+  , deadline      :: CalendarTime             -- | When the Discussion doesn't allow for changing arguments and votes are cast.
+  , participants  :: [(User.UserData, Voted)] -- | Users that take part in the discussion and if they already voted.
+  } deriving (Eq, Show)
 
 class InformationIdentifier i where
   getInformationId :: i -> InformationId
