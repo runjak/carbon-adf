c990c55f3c542de2d44e42355b83ee75ca41527d
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-09-12 22:08:30 +0200

    Wip on Editor
    Ignore-this: a8f5467b749c0e2284a5cf306c154e5
    
    - Basic layout for the editor complete.
    - I need actions for it that must be plugged into javascript.
    - I want it's route to take InformationId as page to edit an Information.
    
    darcs-hash:20120912200830-d180a-c4a53024adb1be1dbc0e4a1c0531075ccb963887

--- OpenBrain/Website/Html/Edit.hs
+++ OpenBrain/Website/Html/Edit.hs
@@ -10,56 +10,79 @@ import OpenBrain.Website.Monad
 import qualified OpenBrain.Website.Html.Decorator as Decorator
 import qualified OpenBrain.Website.Html.Menu as Menu
 
+data EditorContent = EditorContent {
+    editorTitle :: String
+  , title       :: String
+  , description :: String
+  , content     :: String
+  , footerLinks :: [H.Html]
+  }
+emptyContent = EditorContent {
+    editorTitle = "Create Information"
+  , title       = ""
+  , description = ""
+  , content     = "Put some content here."
+  , footerLinks = ["foo","bar"]
+  }
+
 {-
   Expected to be one per page for now.
 -}
-editor :: String -> H.Html
-editor content = H.form ! A.id "editor" $ do
+editor :: EditorContent -> H.Html
+editor eContent = H.form ! A.id "editor" $ do
+  -- Editor headline when given:
+  when (not . null $ editorTitle eContent) $ H.h1 . H.toHtml $ editorTitle eContent
+  H.section $ do
+    -- Input field for the title:
+    H.input ! A.id "InformationTitle" ! A.type_ "text" ! A.placeholder "Place title here" ! (A.value . H.toValue $ title eContent)
+    -- Textarea for the description:
+    H.textarea ! A.id "InformationDescription" ! A.placeholder "Place description here" $ H.toHtml $ description eContent
   -- Toolbar of the editor:
-  let dCommand      = \c -> H.dataAttribute "wysihtml5-command" c
+  let dCommand      = \c -> H.dataAttribute "wysihtml5-command"       c
       dCommandValue = \v -> H.dataAttribute "wysihtml5-command-value" v
+      dDialog       = \d -> H.dataAttribute "wysihtmlw-dialog"        d
+      dDialogAction = \a -> H.dataAttribute "wysihtml5-dialog-action" a
+      dDialogField  = \f -> H.dataAttribute "wysihtml5-dialog-field"  f
   H.header $ H.ul ! A.id "EditorToolbar" ! A.style "display: none;" $ do
     H.li ! dCommand "bold"                              $ "bold"
     H.li ! dCommand "italic"                            $ "italic"
     H.li ! dCommand "createLink"                        $ "link"
     H.li ! dCommand "insertImage"                       $ "image"
-    H.li ! dCommand "insertUnorderedList"               $ "UnorderedList"
-    H.li ! dCommand "insertOrderedList"                 $ "OrderedList"
+    H.li ! dCommand "insertUnorderedList"               $ "ul"
+    H.li ! dCommand "insertOrderedList"                 $ "ol"
     H.li ! dCommand "formatBlock" ! dCommandValue "h1"  $ "h1"
     H.li ! dCommand "formatBlock" ! dCommandValue "h2"  $ "h2"
-    H.li ! dCommand "change_view"                       $ "change view"
+    H.li ! dCommand "change_view"                       $ "html"
     -- Dialog for link creation:
-    H.div ! (H.dataAttribute "wysihtml5-dialog" "createLink") ! A.style "display: none;" $ do
+    H.div ! dDialog "createLink" ! A.style "display: none;" $ do
       H.label $ do
         "Link:"
-        H.input ! (H.dataAttribute "wysihtml5-dialog-field" "href") ! A.value "http://" ! A.class_ "text"
-      H.a ! (H.dataAttribute "wysihtml5-dialog-action" "save") $ "OK"
-      H.a ! (H.dataAttribute "wysihtml5-dialog-action" "cancel") $ "Cancel"
+        H.input ! dDialogField "href" ! A.value "http://" ! A.class_ "text"
+      H.a ! dDialogAction "save"    $ "OK"
+      H.a ! dDialogAction "cancel"  $ "Cancel"
     -- Dialog for image insertion:
-    H.div ! (H.dataAttribute "wysihtml5-dialog" "insertImage") ! A.style "display: none;" $ do
+    H.div ! dDialog "insertImage" ! A.style "display: none;" $ do
       H.label $ do
         "Image:"
-        H.input ! (H.dataAttribute "wyssihtml5-dialog-field" "src") ! A.value "http://"
+        H.input ! dDialogField "src" ! A.value "http://"
       H.label $ do
         "Align:"
-        H.select ! (H.dataAttribute "wysihtml5-dialog-field" "className") $ do
-          H.option ! A.value "" $ "default"
-          H.option ! A.value "img-left" $ "left"
-          H.option ! A.value "img-right" $ "right"
-      H.a ! (H.dataAttribute "wysihtml5-dialog-action" "save") $ "OK"
-      H.a ! (H.dataAttribute "wysihtml5-dialog-action" "cancel") $ "Cancel"
+        H.select ! dDialogField "className" $ do
+          H.option ! A.value ""           $ "default"
+          H.option ! A.value "img-left"   $ "left"
+          H.option ! A.value "img-right"  $ "right"
+      H.a ! dDialogAction "save"    $ "OK"
+      H.a ! dDialogAction "cancel"  $ "Cancel"
   -- The content:
   H.section $ do
     let attrs = [A.id "wysihtml5-textarea"
-                ,A.placeholder "Text input…"
+                ,A.placeholder ""
                 ,A.autofocus "autofocus"]
-    foldl (!) H.textarea attrs $ H.toHtml content
+    foldl (!) H.textarea attrs $ H.toHtml $ content eContent
   -- Buttons to save content, etc.
-  H.footer $ do
-    "Save buttons and thelike."
-    H.a ! A.id "resize" $ "resize"
+  H.footer $ H.ul ! A.id "EditorFooter" $ mapM_ H.li $ footerLinks eContent
 
 serve :: OBW Response
 serve = do
-  p <- Decorator.page $ editor "foo"
+  p <- Decorator.page $ editor emptyContent
   ok $ toResponse p
--- files/js/Edit.js
+++ files/js/Edit.js
@@ -5,15 +5,7 @@ function initEdit(){
   //Load the editor:
   var editor = new wysihtml5.Editor("wysihtml5-textarea", {
     toolbar:      "EditorToolbar"
+  , stylesheets:  "files/css/style.css"
   , parserRules:  wysihtml5ParserRules
   });
-  //Function to resize the iframe:
-  var resize = function(){
-    var frame = $("iframe.wysihtml5-sandbox")[0];
-    var body = frame.contentWindow.document.body;
-    var height = $(body).height();
-    $(frame).height(height);
-    $("#wysihtml5-textarea").height(height);
-  };
-  $("a#resize").click(resize);
 };
