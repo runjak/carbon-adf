f8f2fe71093dd25e63a187be128af7146a1aaa7f
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-11-11 14:02:18 +0100

    Reimplemented Calendartime parameters in OpenBrain.Website.Parameters
    Ignore-this: 7de19022baac7b78f05b8eb160fc6ee3
    
    darcs-hash:20121111130218-d180a-f6252d8b66074e2c8cec693018e16dca2229fca1

--- OpenBrain/Website/Parameters.hs
+++ OpenBrain/Website/Parameters.hs
@@ -4,8 +4,9 @@ module OpenBrain.Website.Parameters where
 -}
 
 import Happstack.Server as S
-import System.Time (CalendarTime)
+import System.Time (CalendarTime(..))
 import Text.Regex as T
+import qualified System.Time as Time
 
 import OpenBrain.Backend.Types
 import OpenBrain.Data.Id
@@ -14,8 +15,12 @@ import OpenBrain.Website.Monad
 getDisplay        = lookRead "display"                                    :: OBW Id
 getLimit          = msum [lookRead "limit", return 30]                    :: OBW Limit
 getOffset         = msum [lookRead "offset", return 0]                    :: OBW Offset
-getAfter          = lookRead "after"                                      :: OBW CalendarTime
-getDeadline       = lookRead "deadline"                                   :: OBW CalendarTime
+getAfter          = fetchCalendarTime
+getDeadline       = do
+  t      <- fetchCalendarTime
+  future <- liftIO $ isFutureTime t
+  guard future
+  return t
 getUser           = liftM fromId $ lookRead "user"                        :: OBW UserId
 getItems          = liftM (map (fromId . wrap) . read) $ look "items"     :: OBW [InformationId]
 getInformationId  = liftM fromId $ lookRead "informationId"               :: OBW InformationId
@@ -26,6 +31,34 @@ getTDC            = liftM3 (,,) getTitle getDescription getContent        :: OBW
 getSplit          = msum [liftM (=="True") $ look "split", return False]  :: OBW Bool
 getStatus         = msum [liftM (=="True") $ look "status", return False] :: OBW Bool -- | For setParticipant
 getDiscussionType = lookRead "discussiontype"                             :: OBW DiscussionType
+
+fetchCalendarTime :: OBW CalendarTime
+fetchCalendarTime = do
+  t      <- liftIO $ Time.toCalendarTime =<< Time.getClockTime
+  year   <- msum [lookRead "year",   return $ Time.ctYear t]
+  month  <- msum [lookRead "month",  return . (+1) . fromEnum $ Time.ctMonth t]
+  day    <- msum [lookRead "day",    return $ Time.ctDay t]
+  hour   <- msum [lookRead "hour",   return $ Time.ctHour t]
+  minute <- msum [lookRead "minute", return $ Time.ctMin t]
+  return $ CalendarTime {
+      ctYear    = year
+    , ctMonth   = toEnum $ month - 1
+    , ctDay     = day
+    , ctHour    = hour
+    , ctMin     = minute
+    , ctSec     = 0
+    , ctPicosec = 0
+    , ctWDay    = toEnum 0
+    , ctYDay    = 0
+    , ctTZName  = "CET"
+    , ctTZ      = 3600
+    , ctIsDST   = False
+    }
+
+isFutureTime :: CalendarTime -> IO Bool
+isFutureTime ct = do
+  t <- Time.toCalendarTime =<< Time.getClockTime
+  return $ ct > t
 
 instance FromReqURI CalendarTime where
   fromReqURI s = case reads s of
