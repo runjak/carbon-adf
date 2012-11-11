1c162a82ca1aca0c9fb0139946816a0ca1cf2b51
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-11-11 12:05:37 +0100

    Default values for Datepicker
    Ignore-this: b4c73315ee74d0b1daef193a70d3bf78
    
    darcs-hash:20121111110537-d180a-4dd2d17efa975b054378a9f5c97d0b59ec0ea4d1

--- OpenBrain/Website/Html/Datepicker.hs
+++ OpenBrain/Website/Html/Datepicker.hs
@@ -1,7 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 module OpenBrain.Website.Html.Datepicker where
 
+import System.Time (CalendarTime(..))
+import Text.Hastache
+import Text.Hastache.Context
+import qualified System.Time as Time
+
 import OpenBrain.Website.Template
 
 datepicker :: IO HTML
-datepicker = tmpl "Datepicker.html" emptyContext
+datepicker = do
+  t <- Time.toCalendarTime =<< Time.getClockTime
+  let date = show (ctYear t) ++ "-"++ (show . (+1) . fromEnum $ ctMonth t)
+                             ++ "-" ++ show (ctDay t + 3)
+      time = show (ctHour t) ++ ":" ++ prettify (ctMin t)
+      context "Date" = MuVariable date
+      context "Time" = MuVariable time
+  tmpl "Datepicker.html" context
+  where
+    prettify = reverse . take 2 . reverse . ('0':) . show
