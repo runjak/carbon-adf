04f3f9b2f8f3fb3d1d22e4aecd792fa3ab39886a
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2012-12-05 15:36:12 +0100

    Changed OpenBrain.Argumentation.Definitions
    Ignore-this: 9ff68019df9e93522c1dd865f5dfc94f
     - Made AF better extendable by changing it to a typeclass instead of a GADT.
     - More magic to follow.
    
    darcs-hash:20121205143612-d180a-b960ecfdf23650a71b5cb5877028c7ae2f653a74

--- OpenBrain/Argumentation/Definitions.hs
+++ OpenBrain/Argumentation/Definitions.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE GADTs #-}
 module OpenBrain.Argumentation.Definitions where
 {-|
   This module contains definitions for
@@ -23,75 +22,49 @@ import qualified Data.Set as Set
 type SimpleAF a = ([a],[(a,a)])
 
 {-|
-  Similar to SimpleAF but with better access times.
-  Also makes sure that arguments contains no duplicates
-  and that no argument is targeted twice.
+  Comparable to SimpleAF, but with better performance.
+  Also nodes' and edges' don't allow for duplicates where SimpleAF does.
 |-}
-data AF a where
-  AF :: Ord a => {
-    nodes :: Set a         -- | The set of Arguments
-  , edges :: Map a (Set a) -- | The set of Relations source->target
-  } -> AF a
-  {-|
-    EAF means Extended ArgumentationFramework
-    and in difference to AF an EAF carries along an annotation function
-    that maps from tuples of Relations between Arguments (Source, Target)
-    to the Annotation type which carries additional information for a relation.
-  |-}
-  EAF :: Ord a => {
-    nodes      :: Set a
-  , edges      :: Map a (Set a)
-  , annotation :: (a, a) -> Annotation
-  } -> AF a
+data AF' a = AF' {
+    nodes' :: Set a         -- | The set of Arguments
+  , edges' :: Map a (Set a) -- | The set of Relations source->target
+  }
+instance AF AF' where
+  nodes = nodes'
+  edges = edges'
 
-instance (Eq a) => Eq (AF a) where
-  af == af' = and [ ((==) `on` nodes) af af'
-                  , ((==) `on` edges) af af']
+{-| Conversion from SimpleAF to AF |-}
+toAF :: (Ord a) => SimpleAF a -> AF' a
+toAF (args, rels) = AF' {
+    nodes' = Set.fromList args
+  , edges' = Map.fromList [(a, Set.fromList [b | (a', b) <- rels, a' == a])|a <- args]
+  }
 
-instance (Show a) => Show (AF a) where
-  show af = let ns = show $ nodes af
-                es = show $ edges af
-            in  "AF {nodes = " ++ ns ++ ", edges = " ++ es ++ "}"
+{-| Conversion from AF to SimpleAF |-}
+toSimpleAF :: AF' a -> SimpleAF a
+toSimpleAF af = (Set.toList $ nodes' af, [(a, b) | (a, b') <- Map.toList (edges' af), b <- Set.toList b'])
+
+{-|
+  The general definition for ArgumentationFrameworks as they'll
+  be used in OpenBrain. I use a typeclass here so that the handling
+  of these Frameworks can be easily extended which will be necessary
+  to allow for defense handling AF or such including values or whatever.
+|-}
+class AF framework where
+  nodes :: framework argument -> Set argument
+  edges :: framework argument -> Map argument (Set argument)
 
 {-| Fetches a list of arguments from an ArgumentationFramework. |-}
-arguments :: AF a -> Set a
+arguments :: (AF f) => f a -> Set a
 arguments = nodes
 
 {-| Fetches all targets for a single argument |-}
-targets :: (Ord a) => AF a -> a -> Set a
+targets :: (AF f, Ord a) => f a -> a -> Set a
 targets f a = maybe Set.empty id . Map.lookup a $ edges f
 
 {-| Fetches all relations from an ArgumentationFramework |-}
-relations :: (Ord a) => AF a -> Set (a, a)
+relations :: (AF f, Ord a) => f a -> Set (a, a)
 relations f = let args = arguments f
                   rels = Set.map (\a -> Set.map ((,) a) $ targets f a) args
               in  Set.unions $ Set.toList rels
 
-{-| Conversion from SimpleAF to AF |-}
-toAF :: (Ord a) => SimpleAF a -> AF a
-toAF (args, rels) = AF {
-    nodes = Set.fromList args
-  , edges = Map.fromList [(a, Set.fromList [b | (a', b) <- rels, a' == a])|a <- args]
-  }
-
-{-| Conversion from AF to SimpleAF |-}
-toSimpleAF :: AF a -> SimpleAF a
-toSimpleAF af = (Set.toList $ nodes af, [(a, b) | (a, b') <- Map.toList (edges af), b <- Set.toList b'])
-
-{-| Annotations to be used in EAF's |-}
-data Annotation = Attack
-                | Defense
-                deriving (Show, Eq, Ord, Enum)
-
-{-| Enhances an AF a with an annotation function so that it's an EAF. |-}
-annotate :: AF a -> ((a, a) -> Annotation) -> AF a
-annotate (AF n e)     f = EAF n e f -- | Sets the annotation function f
-annotate (EAF n e f') f = EAF n e f -- | Overwrites the annotation functionn f' with f
-
-{-|
-  Makes sure a given AF is an EAF by defaulting the annotation to Attack
-  for all relations if the AF is not already an EAF.
-|-}
-mkEAF :: AF a -> AF a
-mkEAF af@(EAF _ _ _) = af
-mkEAF af@(AF _ _)    = annotate af $ const Attack
