cb566f5dfdc51c98afc1a81a571ca56dc8f2b829
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2013-03-29 14:32:49 +0100

    Removed debug logs from UserView.js
    Ignore-this: 45090c8e1ad1d5614d5de6185876476d
     - UserView should stick to displaying single users instead of allowing to browse them
     - It also needs some rewriting because the Accordion doesn't work as expected
    
    darcs-hash:20130329133249-d180a-bd05516554b49b13c2f001d485660fbf14a50a8d

--- files/js/views/UserView.js
+++ files/js/views/UserView.js
@@ -1,6 +1,5 @@
 UserView = Backbone.View.extend({
   initialize: function(){
-    console.log('UserView:initialize()');
     this.ucol = new UserCollection();
     this.listenTo(this.ucol, "reset", this.render);
     var t = this;
@@ -14,7 +13,6 @@ UserView = Backbone.View.extend({
     this.acc.accordion();
   }
 , render: function(){
-    console.log('UserView:render()');
     var t = this;
     this.acc.html('').accordion('destroy');
     this.ucol.each(function(user){
@@ -23,7 +21,6 @@ UserView = Backbone.View.extend({
     t.acc.accordion();
   }
 , fetch: function(){
-    console.log('UserView:fetch()');
     var q = { limit:  this.pager.getLimit()
             , offset: this.pager.getOffset() };
     var ucol = this.ucol;
@@ -32,13 +29,11 @@ UserView = Backbone.View.extend({
     });
   }
 , buildUserEntry: function(user){
-    console.log('UserView:buildUserEntry()');
     var title   = user.get('id')+": "+user.get('username');
     var content = JSON.stringify(user.attributes);
     return "<h3>"+title+"</h3>"+"<div>"+content+"</div>";
   }
 , setSpecific: function(user){
-    console.log('UserView:setSpecific()');
     this.ucol.add(user, {at: 0});
     this.render();
   }
