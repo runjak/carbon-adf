6699c909eea9d06a9aa70eb08a88b78a5a8aacef
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2013-03-31 19:23:16 +0200

    Bugfix in models/CurrentInformation.js
    Ignore-this: cc2a721f0c0473a7e3afe149dca55a94
    
    darcs-hash:20130331172316-d180a-726ac1c96d459896c5cbfc89163d3e409e78fbb5

--- files/js/models/CurrentInformation.js
+++ files/js/models/CurrentInformation.js
@@ -9,6 +9,7 @@ CurrentInformation = Information.extend({
 , loGetId: function(){ return localStorage.currentInformation; }
 , loSetId: function(id){ localStorage.currentInformation = id; }
 , setId:   function(id){
+    if(id === this.get('id')) return;
     var t = this;
     var i = new Information({id: id});
     i.fetch().done(function(){
@@ -18,5 +19,6 @@ CurrentInformation = Information.extend({
 , replaceWith: function(i){
     this.attributes = {};
     this.set(i.attributes);
+    this.loSetId(this.get('id'));
   }
 });
