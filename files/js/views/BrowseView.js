0965082c87b4d679807adcf88c910f4fcfe0b197
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2013-03-31 22:00:13 +0200

    Added actions to views/BrowseView.js
    Ignore-this: b13088da818e19216d531f075e2fbe6a
     - Now Informations can be collected and uncollected
     - It doesn't currently recognize already collected informations correctly.
    
    darcs-hash:20130331200013-d180a-7d0e0dd4ed0d11f15ac220f3f90af592e94401b5

--- files/js/views/BrowseView.js
+++ files/js/views/BrowseView.js
@@ -26,14 +26,37 @@ BrowseView = Backbone.View.extend({
       , i.get('description')
       , '<a href="#users/' + i.get('author').id + '">' + i.get('author').username + '</a>'
       , i.get('creation')
-      , (function(d){if(!d) return ""; return d})(i.get('deletion'))
+      , (function(d){return d ? d : "";})(i.get('deletion'))
+      , (function(i){
+          return '<div data-iid="' + i + '">'
+               + '<button class="collectInformation"><span class="ui-icon ui-icon-disk" '
+               + 'title="Collect this Information"></span></button>'
+               + '<button class="uncollectInformation"><span class="ui-icon ui-icon-close" '
+               + 'title="Remove this Information from the pool of collected Informations"></span></button>'
+               + '</div>';
+        })(i.get('id'))
       ];
       var content = _.reduce(cells, function(m, c){return m + "<td>" + c + "</td>";}, "");
       display.append("<tr>" + content + "</tr>");
     });
+    //Binding links:
     display.find('a').click(function(){
       var target = $(this).attr('href');
       window.App.router.navigate(target, {trigger: true});
+    });
+    //Binding buttons:
+    this.col.each(function(i){
+      var col = window.App.collectedInformations;
+      var div = display.find('div[data-iid="'+i.get('id')+'"]');
+      var cI = div.find('button.collectInformation');
+      var uI = div.find('button.uncollectInformation');
+      if(col.contains(i)){ cI.hide(); }else{ uI.hide(); }
+      cI.click(function(){
+        cI.hide(); col.add(i); uI.show();
+      });
+      uI.click(function(){
+        uI.hide(); col.remove(i); cI.show();
+      });
     });
   }
 });
