910bd38376ffe0d3ce5eae9c15fb58431235d4f1
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2013-05-08 14:06:16 +0200

    Added OpenBrain.Backend:tryBackend
    Ignore-this: 4f5b49469a3f352ad2b75c6cab4fe425
     - This function can be used from ghci to test the DSL manually
    
    darcs-hash:20130508120616-d180a-0626e2bd5e939d40eb873aea35de9de43ce531fd

--- OpenBrain/Backend.hs
+++ OpenBrain/Backend.hs
@@ -6,12 +6,23 @@ import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
 
-import OpenBrain.Backend.DSL 
-import OpenBrain.Config
-import OpenBrain.Common
+import OpenBrain.Backend.DSL as DSL
+import OpenBrain.Config      as Config
+import OpenBrain.Common      as Common
 import qualified OpenBrain.Backend.PostgreSQL as Psql
 
 loadBackend :: Config -> MaybeT IO CBackendProcessor
 loadBackend c
   | Psql.validConfig c = liftIO $ Psql.load c
   | otherwise          = mzero
+
+tryBackend :: FilePath -> IO (BackendDSL r -> IO r)
+tryBackend path = do
+  mconf <- Config.readConfig path
+  case mconf of
+    Nothing  -> error "Could not load Config."
+    (Just c) -> do
+      mback <- runMaybeT $ loadBackend c
+      case mback of
+        Nothing  -> error "Could not load Backend."
+        (Just b) -> return $ process b
